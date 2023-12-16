38ba813dc35b48fdad159b04a39c96e484610294
DoYuraPlay <128250310+DoYuraPlay@users.noreply.github.com>
2023-12-16 15:33:42 +0700

    Add files via upload

---
+++ Абдул18.py
@@ -0,0 +1,32 @@
+import time
+while True:
+    print("Ведите пароль:")
+    par=input("")
+    if par=="Ёлки иголки":
+       print("Доступ разрешён.")
+       print("Добро пожаловать")
+       time.sleep(1)
+       print("-А на каком я сайте?")
+       time.sleep(1)
+       print("Por...")
+       time.sleep(1)
+       print("-Я понел.")
+       time.sleep(1)
+       print("*Выход*")
+       time.sleep(1)
+    elif par=="Лололошка топ":
+       print("Это да, но это не пароль")
+       print("Доступ отклонён")
+    if par=="Я незнаю":
+       print("Блин я теперь забыл его(")
+    elif par=="Смешарики топ":
+       print("Согласен тебе разрешён доступ.")
+       print("Добро пожаловать")
+    if par=="Снег":
+       print("...")
+       print("Неа лёд")
+       print("Доступ отклонён")
+    elif par=="Скибиди доп ес":
+       print("Туалет, чтоли?")
+       print("Ща камеру достану")
+       print("Доступ отклонён")
